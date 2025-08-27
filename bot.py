9df0c398fd2a032c64015c651942be411ae9f23c
muzzumz69-crypto <muzzumz69@gmail.com>
2025-08-27 10:08:41 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,9 +4,9 @@ import random
 from pathlib import Path
 import threading
 
-import discord
-from discord import app_commands
-from discord.ext import commands
+import disnake as discord
+from disnake import app_commands
+from disnake.ext import commands
 from dotenv import load_dotenv
 
 from flask import Flask, render_template_string
